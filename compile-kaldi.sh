6eb6a7793334bb7b413f5448fd7da396a37de056
Phuong Quang Than <64473652+garenaobama@users.noreply.github.com>
2025-11-17 09:40:20 +0700

    Update compile-kaldi.sh

--- compile-kaldi.sh
+++ compile-kaldi.sh
@@ -4,7 +4,7 @@ cd ${WORKING_DIR}/kaldi/tools
 
 # tools directory --> we'll only compile OpenFST
 OPENFST_VERSION=$(grep -oP "OPENFST_VERSION *\?= *\K(.*)$" ${WORKING_DIR}/kaldi/tools/Makefile)
-wget -T 10 -t 1 http://openfst.cs.nyu.edu/twiki/pub/FST/FstDownload/openfst-${OPENFST_VERSION}.tar.gz
+wget -T 10 -t 1 https://www.openfst.org/twiki/pub/FST/FstDownload/openfst-${OPENFST_VERSION}.tar.gz
 tar -zxvf openfst-${OPENFST_VERSION}.tar.gz
 cd openfst-${OPENFST_VERSION}/
 CXX=clang++ ./configure --prefix=`pwd` --enable-static --enable-shared --enable-far --enable-ngram-fsts --host=arm-linux-androideabi LIBS="-ldl"
